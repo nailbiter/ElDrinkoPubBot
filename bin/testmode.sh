11440eafe8163a0a4bc98ab4147ba0616f99e17b
Alex Leontiev <nailbiter@dtws-work.in>
2020-05-17 13:47:30 +0900

    stash

--- bin/testmode.sh
+++ bin/testmode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="ProtoElDrinkoPubBot" -DAPP_LOG_ROOT=.log/ProtoElDrinkoPubBot
+mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="ProtoElDrinkoPubBot" -Dfilename=ProtoElDrinkoPubBot.txt
