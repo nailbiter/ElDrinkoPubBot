b899d84217348f9c8aa68b5e0a21a266fd4f57f5
Alex Leontiev <nailbiter@dtws-work.in>
2020-05-18 01:01:35 +0900

    good

--- bin/testmode.sh
+++ bin/testmode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="ProtoElDrinkoPubBot" -Dfilename=ProtoElDrinkoPubBot.txt
+mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="ProtoElDrinkoPubBot" -Dfilename=ProtoElDrinkoPubBot
