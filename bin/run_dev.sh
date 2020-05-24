b3c96e1f28d24aba6822ae640623f7afc743804b
Alex Leontiev <alozz1991@gmail.com>
2020-05-24 22:04:18 +0900

    fix (#27)
    Co-authored-by: Alex Leontiev <nailbiter@dtws-work.in>

--- bin/run_dev.sh
+++ bin/run_dev.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+
 BOT_NAME=DevElDrinkoPubBot
+COMMIT_MSG=`git log -1 --pretty=%B|head -n1`
+COMMIT_HASH=`git rev-parse --short HEAD`
+COMMIT_DATA="\`$COMMIT_MSG\` (*$COMMIT_HASH*)"
+
 
 mvn clean
 mvn install
-mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="$BOT_NAME" -Dfilename=$BOT_NAME
+mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="\"$COMMIT_DATA\" $BOT_NAME" -Dfilename=$BOT_NAME
--- bin/testmode.sh
+++ bin/testmode.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-git checkout staging
+
+BOT_NAME=ProtoElDrinkoPubBot
+COMMIT_MSG=`git log -1 --pretty=%B|head -n1`
+COMMIT_HASH=`git rev-parse --short HEAD`
+COMMIT_DATA="\`$COMMIT_MSG\` (*$COMMIT_HASH*)"
+BRANCH=staging
+
+
+git checkout $BRANCH
 git pull
 mvn clean
 mvn install
-mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="ProtoElDrinkoPubBot" -Dfilename=ProtoElDrinkoPubBot
+mvn exec:java -Dexec.mainClass="nl.insomnia247.nailbiter.eldrinkopubbot.App" -Dexec.args="\"$COMMIT_DATA\" $BOT_NAME" -Dfilename=$BOT_NAME
