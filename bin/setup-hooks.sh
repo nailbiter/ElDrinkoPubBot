2b35881cc7511e89291800aaaf957dc9b65f2e49
Alex Leontiev <nailbiter@dtws-work.in>
2020-08-21 11:30:17 +0900

    cleanup

--- bin/setup-hooks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ln -s `pwd`/bin/pre-push .git/hooks/; ln -s `pwd`/bin/pre-commit .git/hooks/;
--- src/main/java/nl/insomnia247/nailbiter/eldrinkopubbot/model/OutputMessage.java
+++
@@ -1,5 +0,0 @@
-package nl.insomnia247.nailbiter.eldrinkopubbot.model;
-import nl.insomnia247.nailbiter.eldrinkopubbot.model.Jsonable;
-
-public interface OutputMessage extends Message, Jsonable {
-}
