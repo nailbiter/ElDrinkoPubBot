43b1853a945734f7cce58b708abb858828e8733e
Alex Leontiev <nailbiter@dtws-work.in>
2020-12-19 18:16:20 +0900

    commit

---
+++ _beerserver.py
@@ -0,0 +1,56 @@
+"""===============================================================================
+
+        FILE: _beerserver.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2020-12-19T18:12:29.921622
+    REVISION: ---
+
+==============================================================================="""
+from pymongo import MongoClient
+import logging
+from datetime import datetime
+
+def add_logger(f):
+    logger = logging.getLogger(f.__name__)
+
+    def _f(*args, **kwargs):
+        return f(*args, logger=logger, **kwargs)
+    _f.__name__ = f.__name__
+    return _f
+
+def get_mongo_client():
+    with open("secret.txt") as f:
+        mongo_pass = f.read().strip()
+    mongo_client = MongoClient(
+        f"mongodb+srv://nailbiter:{mongo_pass}@cluster0-ta3pc.gcp.mongodb.net/beerbot?retryWrites=true&w=majority")
+    return mongo_client
+
+
+@add_logger
+def get_orders(date,logger=None):
+    mongo_client = get_mongo_client()
+
+    if date:
+        logger.info(date)
+        _date = datetime.strptime(date, "%Y-%m-%d")
+        regx = f".*{_date.strftime('%d.%m.%y')}.*"
+        logger.info(regx)
+        search_object = {"order.timestamp": {"$regex": regx}}
+    else:
+        search_object = {}
+
+    res = [{**{k: v for k, v in o["order"].items() if k != "_id"}, "_timestamp":o["order"]["_timestamp"].strftime("%d.%m.%y %H:%M")}
+           for o
+           in mongo_client.beerbot.order_history.find(filter=search_object, sort=[("order._timestamp", -1)])]
+    return res
--- beerserver.py
+++ beerserver.py
@@ -5,6 +5,7 @@ import re
 from pymongo import MongoClient
 import pandas as pd
 import logging
+from _beerserver import get_mongo_client, get_orders
 
 
 # global var's
@@ -12,49 +13,31 @@ logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
 # procedures
-def _get_mongo_client():
-    with open("secret.txt") as f:
-        mongo_pass = f.read().strip()
-    mongo_client = MongoClient(
-        f"mongodb+srv://nailbiter:{mongo_pass}@cluster0-ta3pc.gcp.mongodb.net/beerbot?retryWrites=true&w=majority")
-    return mongo_client
-def _get_orders(date):
-    logger = logging.getLogger("_get_orders")
-    mongo_client = _get_mongo_client()
-    
-    if date:
-        logger.info(date)
-        _date = datetime.strptime(date,"%Y-%m-%d")
-        regx = f".*{_date.strftime('%d.%m.%y')}.*"
-        logger.info(regx)
-        search_object = {"order.timestamp":{"$regex":regx}}
-    else:
-        search_object = {}
 
-    res = [{**{k:v for k,v in o["order"].items() if k!="_id"}, "_timestamp":o["order"]["_timestamp"].strftime("%d.%m.%y %H:%M")} 
-            for o 
-            in mongo_client.beerbot.order_history.find(filter=search_object,sort=[("order._timestamp",-1)])]
-    return res
 
-@app.route("/refresh_db",methods=["POST"])
+
+
+@app.route("/refresh_db", methods=["POST"])
 def refresh_db():
-    mongo_client = _get_mongo_client()
+    mongo_client = get_mongo_client()
     for cn in ["proto_data"]:
         coll = mongo_client.beerbot[cn]
-        coll.delete_one({"id":"145766172"})
+        coll.delete_one({"id": "145766172"})
     return "done"
+
+
 @app.route('/')
 @app.route('/<date>')
 def hello_world(date=None):
     if date is None:
         date = datetime.now().strftime("%Y-%m-%d")
-    elif date=="all":
+    elif date == "all":
         date = None
-    elif date=="refresh_db":
+    elif date == "refresh_db":
         return render_template("refresh_db.jinja.html")
     elif date != "all" and match(r"^\d{4}-\d{2}-\d{2}$", date) is None:
         return f"""date should be in the format "YYYY-MM-DD" (or absent)!<br>(received: "{date}")"""
     else:
         raise NotImplementedError
-    orders = _get_orders(date)
-    return render_template("table.jinja.html",table=pd.DataFrame(orders).to_html(index=False) if orders else "none")
+    orders = get_orders(date)
+    return render_template("table.jinja.html", table=pd.DataFrame(orders).to_html(index=False) if orders else "none")
