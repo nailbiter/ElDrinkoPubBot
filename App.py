c3ade6626d03f491861914371ac1787c1c670d86
Alex Leontiev <nailbiter@dtws-work.in>
2021-01-31 17:33:53 +0900

    stash

---
+++ App.py
@@ -0,0 +1,23 @@
+"""===============================================================================
+
+        FILE: App.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-01-31T17:12:33.975745
+    REVISION: ---
+
+==============================================================================="""
+
+
+if __name__ == "__main__":
+    pass
