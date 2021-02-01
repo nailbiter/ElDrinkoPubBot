13b800515da2b2a9b55b1b30227a6e8eebcece9f
Alex Leontiev <alex@home.raspberry>
2021-02-01 23:37:42 +0900

    stash

--- App.py
+++ App.py
@@ -22,28 +22,45 @@ ORGANIZATION:
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 from pymongo import MongoClient
-from os import environ
 import click
 import json
+import os
+from os import path
+import atexit
+from nl.insomnia247.nailbiter.eldrinkopubbot.eldrinko.ElDrinkoPubBot import ElDrinkoPubBot
+
+class _AtExitHook:
+    def __init__(self,pidfile):
+        self._pidfile = pidfile
+    def __call__(self):
+        click.echo("_atexit_hook")
+        os.system(f"rm -rf {self._pidfile}")
 
 @click.command()
 @click.option("--mongo-url",envvar="MONGO_URL")
 @click.option("--mongo-url",envvar="MONGO_URL")
 @click.option("--environment",type=click.Choice(["ElDrinkoPubBot","ProtoElDrinkoPubBot","DevElDrinkoPubBot"]),default="DevElDrinkoPubBot")
 def App(mongo_url,environment):
+    pidfile = f".tmp/{environment}.txt"
+    assert not path.isfile(pidfile), "only one instance of ElDrinkoPubBot allowed to run"
+    with open(pidfile,"w") as f:
+        f.write(str(os.getpid()))
     mongo_client = MongoClient(mongo_url)
     settings,keyring = [
             {k:v for k,v in mongo_client.beerbot[kk].find_one({"id":environment}).items() if k not in ["_id","id"]} 
             for kk
             in ["_settings","_keyring"]
     ]
-#    print(json.dumps(settings,indent=2))
-#    print(json.dumps(keyring,indent=2))
-    telegram_token = keyring["telegram"]["token"]
-    updater = Updater(token, use_context=True)
-    bot = updater.bot
-    updater.dispatcher.add_handler(MessageHandler(
-        filters=Filters.all, callback=_message_handler))
+    click.echo(json.dumps(settings,indent=2))
+    click.echo(json.dumps(keyring,indent=2))
+    atexit.register(_AtExitHook(pidfile))
+    click.echo("here")
+
+#    telegram_token = keyring["telegram"]["token"]
+#    updater = Updater(token, use_context=True)
+#    bot = updater.bot
+#    updater.dispatcher.add_handler(MessageHandler(
+#        filters=Filters.all, callback=ElDrinkoPubBot()))
 
 if __name__=="__main__":
     App()
---
+++ nl/insomnia247/nailbiter/eldrinkopubbot/eldrinko/ElDrinkoPubBot.py
@@ -0,0 +1,25 @@
+"""===============================================================================
+
+        FILE: nl/insomnia247/nailbiter/eldrinkopubbot/ElDrinkoPubBot.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-02-01T23:19:03.703452
+    REVISION: ---
+
+==============================================================================="""
+
+class ElDrinkoPubBot:
+    def __init__(self):
+        pass
+    def __call__(self,update,context):
+        pass
