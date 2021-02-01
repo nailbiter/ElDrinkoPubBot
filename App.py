84768ab6fbc2e1c66f112651ee2d005913ac39d0
Alex Leontiev <alex@home.raspberry>
2021-02-01 22:26:21 +0900

    stash

---
+++ App.py
@@ -0,0 +1,25 @@
+"""===============================================================================
+
+        FILE: app.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2021-02-01T22:21:46.342576
+    REVISION: ---
+
+==============================================================================="""
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from pymongo import MongoClient
+
+if __name__=="__main__"
