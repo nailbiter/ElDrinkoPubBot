db94268c7b6fab724a129cc98c79b6b9388a588c
Alex Leontiev <alozz1991@gmail.com>
2021-02-12 21:59:37 +0900

    Update beerserver.py

--- beerserver.py
+++ beerserver.py
@@ -96,7 +96,7 @@ def add_beeritem():
                            r={
                                **{k: "text" for k in r if k not in ["category", "units"]},
                                "category": [r["name"] for r in mongo_client.beerbot.proto_categories.find()],
-                               "unit": ["литры","штуки","пачки"]
+                               "unit": ["литры","штуки","пачки"] #FIXME: take from db: `units`
                            },
                            action="added_beeritem"
                            )
