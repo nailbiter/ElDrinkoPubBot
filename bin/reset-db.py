0c79f765b9dee04150894eeff8b06d8d4652b6a0
Alex Leontiev <nailbiter@dtws-work.in>
2021-02-18 19:03:54 +0900

    stash

--- bin/reset-db.py
+++ bin/reset-db.py
@@ -23,14 +23,19 @@ import click
 from pymongo import MongoClient
 import itertools
 
+_USERS = {
+    "me": "340880765",
+    "dad": "145766172"
+}
+
 
 @click.command()
-@click.option("-c", "--chat-id", default=340880765, type=int)
+@click.option("-c", "--chat-id", default="me", type=click.Choice(list(_USERS)))
 @click.option("--mongo-url", envvar="MONGO_URL")
 def reset_db(chat_id, mongo_url):
     client = MongoClient(mongo_url)
-    for p,cn in itertools.product(["","proto_","dev_"],["data","state_machine_states"]):
-        client.beerbot[p+cn].delete_one({"id":str(chat_id)})
+    for p, cn in itertools.product(["", "proto_", "dev_"], ["data", "state_machine_states"]):
+        client.beerbot[p+cn].delete_one({"id": _USERS[chat_id]})
 
 
 if __name__ == "__main__":
