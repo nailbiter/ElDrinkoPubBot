23ad30f53b77dfb80c8f71dbd76271939d0d127f
Oleksii Leontiev <alozz1991@gmail.com>
2022-11-26 19:15:21 +0900

    fixup

--- scripts-get-beerlist.py
+++ scripts-get-beerlist.py
@@ -22,14 +22,20 @@ ORGANIZATION:
 import click
 from py.util import google_spreadsheet
 import os
+from jinja2 import Template
+
+_SCP_TEMPLATE = Template(
+    """scp -P {{port}} .token.json 0.tcp.ap.ngrok.io:~/Dokumente/forgithub/ElDrinkoPubBot/""")
 
 
 @click.command()
-def scripts_get_beerlist():
+@click.option("--port", "-p", required=True, type=int)
+def scripts_get_beerlist(port):
     creds = google_spreadsheet.get_creds(
         client_secret_file="client_secret.json", create_if_not_exist=True)
-    os.system("scp -i ~/.ssh/kimgym.pem .token.json ec2-user@ec2-3-143-68-209.us-east-2.compute.amazonaws.com:~/eldrinko-master")
-    os.system("scp -i ~/.ssh/kimgym.pem .token.json ec2-user@ec2-3-143-68-209.us-east-2.compute.amazonaws.com:~/eldrinko-staging")
+
+    os.system(_SCP_TEMPLATE.render({"port": port}))
+#    os.system("scp -i ~/.ssh/kimgym.pem .token.json ec2-user@ec2-3-143-68-209.us-east-2.compute.amazonaws.com:~/eldrinko-staging")
 
 
 if __name__ == "__main__":
